a3440fc12cf26bb8007db6be5e242579ef334ab2
Santi Santos <santos.santi@gmail.com>
2018-04-01 21:10:28 +0200

    Rutas Agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { app_routing } from "./app.routes";
 
+//Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -17,10 +20,11 @@ import { ItemComponent } from './components/item/item.component';
     FooterComponent,
     PortafolioComponent,
     AboutComponent,
-    ItemComponent
+    ItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing    
   ],
   providers: [],
   bootstrap: [AppComponent]
