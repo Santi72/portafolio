4a5f512a207c62515bb4049d9ee7301fdb3cdbe9
Santi Santos <santos.santi@gmail.com>
2018-04-01 22:14:41 +0200

    Modificaciones al menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent,PortafolioComponent,ItemComponent } from './components/index.paginas';
 
 const app_routes: Routes = [
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'item', component: ItemComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 
