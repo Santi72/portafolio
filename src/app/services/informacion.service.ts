6c4f336d6520ee2859cbbc308395895579555f4f
Santi Santos <santos.santi@gmail.com>
2018-04-12 21:01:00 +0200

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,18 +5,38 @@ import { Http } from "@angular/http";
 export class InformacionService {
 
   info:any= {};
-  cargada:boolean = false;
+  cargada: boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any[] = [];
 
   constructor(public http:Http) {
 
-    this.http.get("assets/data/info.pagina.json")
-              .subscribe( data=>{
-                console.log(data.json());
+      this.carga_info();
+      this.carga_sobre_nosotros();
 
-                this.cargada = true;
-                this.info=data.json();
-              }
-              )
+   }
+
+   public carga_info(){
+     this.http.get("assets/data/info.pagina.json")
+       .subscribe(data => { 
+
+         this.cargada = true;
+         this.info = data.json();
+       }
+       )
+
+   }
+
+   public carga_sobre_nosotros(){
+     this.http.get("https://webangulartemplate.firebaseio.com/equipo.json")
+       .subscribe(data => {
+         console.log(data.json());
+
+         this.cargada_sobre_nosotros = true;
+         this.equipo = data.json();
+       }
+       )
+
 
    }
 
